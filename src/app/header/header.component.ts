1beba8a4114b6bdec0e86c6842f74ccbfd95356c
LAB_B_REAL <labb@continental.edu.pe>
2019-08-21 21:27:36 -0500

    bejavet sapchet

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService,SocialUser } from "angularx-social-login";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  private usuario: SocialUser;
+  private loggeIn:boolean;
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authService.authState.subscribe((usuario)=>{
+      this.usuario=usuario;
+      this.loggeIn=(usuario != null);
+    })
   }
 
 }
